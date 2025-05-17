e63e02ff3972cfaba14bd2fce9d4c85991c23ad3
Sachidanand Prasad Sah <113513747+Sachidanand-sah@users.noreply.github.com>
2025-05-17 15:00:22 +0545

    added resposiveness in mobile view

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const sidebar = document.querySelector('.sidebar');
-    sidebar.addEventListener('click', () => {
-        if (window.innerWidth <= 768) {
-            sidebar.classList.toggle('expanded');
-        }
-    });
+document.addEventListener("DOMContentLoaded", () => {
+  const menuToggle = document.getElementById("menuToggle");
+  const sidebar = document.getElementById("sidebar");
+
+  menuToggle.addEventListener("click", () => {
+    sidebar.classList.toggle("open");
+  });
+
+  
+  document.addEventListener("click", (e) => {
+    if (
+      sidebar.classList.contains("open") &&
+      !sidebar.contains(e.target) &&
+      !menuToggle.contains(e.target)
+    ) {
+      sidebar.classList.remove("open");
+    }
+  });
 });
